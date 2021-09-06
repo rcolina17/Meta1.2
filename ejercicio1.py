65f3408ade775af91addab795fa8da32fa5695a3
rcolina17 <89890764+rcolina17@users.noreply.github.com>
2021-09-06 16:59:37 -0700

    Update ejercicio1.py

--- ejercicio1.py
+++ ejercicio1.py
@@ -6,16 +6,4 @@ if num>=1 and num<=10:
 else: print('limite')
 
 
-"""if num% 2==0:
-  print(num,"Es par") 
-else: 
-  print(num,"Es impar ")"""
 
-  """num=random.randrange(1,50)
-a= int(input("ESCRIBE UN NUMERO DEL 1 AL 50: "))
-print (num)
-while a != num:
-    if a>50 or a<0: print("PORFAVOR INGRESAR UN NUMERO QUE SE ENCUENTRE ENTRE EL 1 AL 50")
-    elif a<num: a= int (input("UN NUMERO MAS ALTO, INGRESE UN NUMERO ENTRE EL 1 AL 50"))
-    elif a>num: a= int(input("UN NUMERO MAS BAJO,INGRESE UN NUMERO ENTRE EL 1 AL 50"))
-else: print("ES TODO HAZ GANADO")"""
