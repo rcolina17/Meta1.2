49446847a5f302dea0d77fc0b955d9f98b9a00be
rcolina17 <89890764+rcolina17@users.noreply.github.com>
2021-09-06 19:39:03 -0700

    Correccion

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,9 +1,11 @@
 print('Bienvenido')
 num=int(input('Dame un numero del 1 al 1000:  '))
-if num>=1 and num<=10:
-  if num% 2==0:print(num,"Es par") 
-  if not print(num,"Es impar "):
-else: print('limite')
+if num% 2==0:
+  print(num,"Es par") 
+else:  
+  print(num,"Es impar ") 
+
+
 
 
 
