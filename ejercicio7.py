eeda957b25a80f8b49e6b36d6e647656f9911d9f
rcolina17 <89890764+rcolina17@users.noreply.github.com>
2021-09-13 14:20:29 -0700

    Correccion

--- ejercicio7.py
+++ ejercicio7.py
@@ -1,11 +1,13 @@
 #adivina el numero
 import random
 maquina=random.randrange(1,50)
-print(maquina)
+# print(maquina)
 jugador=int(input('Dame un numero del 1 al 50: '))
 while jugador !=maquina:
-    if jugador<maquina: jugador=int(input ('Dame un numero mas grande '))
-    elif jugador>maquina: jugador= int(input ('Dame un numero mas pequeno '))
+    if jugador<maquina: 
+        jugador=int(input ('Dame un numero mas grande '))
+    elif jugador>maquina: 
+        jugador= int(input ('Dame un numero mas pequeno '))
 else: print('Adivinaste el numero')
 
 
